7750fdc105ccec35db4ae9ca30d1b37739602a12
bruceSz <zsyuyizhang@gmail.com>
2013-01-13 21:43:50 +0800

    test

--- python/myserver.py
+++ python/myserver.py
@@ -6,7 +6,7 @@ PORT = 8000
 
 reply='Yes'
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.bind(HOST,PORT)
+s.bind((HOST,PORT))
 s.listen(3)
 conn,addr = s.accept()
 request = conn.recv(1024)
