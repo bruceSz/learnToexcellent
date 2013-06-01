853e526ab61f64383507cb834764f64c58e406c9
bruceSz <zsyuyizhang@gmail.com>
2013-06-01 10:34:43 +0800

    modified:   python/flaskr/flaskr.py 	modified:   python/flaskr/templates/show_entries.html 	deleted:    shell/largeNumber.sh

--- python/flaskr/flaskr.py
+++ python/flaskr/flaskr.py
@@ -43,6 +43,21 @@ def login():
     flash('automatically logged in.')
     return redirect(url_for('show_entries'))
 
+@app.route('/add',methods=['post'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('insert into entries (title,text) values (?,?)',[request.form['title'],request.form['text']])
+    g.db.commit()
+    flash('New entry was successfully posted.')
+    return redirect(url_for('show_entries'))
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in',None)
+    flash('you were logged out')
+    return redirect(url_for('show_entries'))
+
 @app.route('/')
 def show_entries():
     cur = g.db.execute('select title,text from entries order by id desc')
