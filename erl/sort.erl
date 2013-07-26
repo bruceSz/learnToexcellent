02163a9d9bde42cd6cb8e1fe8ae14d25392504bf
bruceSz <zsyuyizhang@gmail.com>
2013-07-26 18:01:35 +0800

    modified:   sort.erl

--- erl/sort.erl
+++ erl/sort.erl
@@ -1,14 +1,31 @@
 -module(sort).
--export([select_sort/1]).
+-export([find_max/1,select_sort/1]).
+
 %% find max
 find_max(L)->
 	H = hd(L),
 	T = tl(L),
+	do_find_max(H,T,[]).
+
+do_find_max(Max,[],L)->
+	{Max,L};
+
+do_find_max(Max,[H|T],L) when Max>=H ->
+	do_find_max(Max,T,[H|L]);
+
+do_find_max(Max,[H|T],L) when Max<H ->
+	do_find_max(H,T,[Max|L]).
 
 select_sort(L)->
-	H = hd(L),
-	T = tl(L),
-	select_sort(L,[]).
+	do_select_sort(L,[]).
 
-select_sort(L,Result)->
+do_select_sort([],R)->
+	R;
+do_select_sort(L,R)->
+	{Max,T}=find_max(L),	
+	io:format("|~w|",[Max]),
+	do_select_sort(T,[Max|R]).
+
+
+	
 	
