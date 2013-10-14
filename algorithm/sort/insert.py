40a531c6d99e9e04571ad9966500f609f08a761b
bruceSz <zsyuyizhang@gmail.com>
2013-10-14 21:19:06 +0800

    modified:   algorithm/sort/insert.py 	algorithm/sort/divide_conquer.py 	algorithm/sort/heap.py 	algorithm/sort/merge.py 	algorithm/sort/quick.py

--- algorithm/sort/insert.py
+++ algorithm/sort/insert.py
@@ -6,7 +6,7 @@ def insert_sort(l):
 	cur = l[i]
 	j = i-1
 	while j>=0 and l[j]>cur:
-	    l[j],l[j+1] = l[j+1],l[j]
+	    l[j+1] = l[j]
 	    j=j-1
 	    
 	l[j+1]=cur
@@ -14,7 +14,7 @@ def insert_sort(l):
     return l
 	
 if __name__ == '__main__':
-    l=[67,99,2,45,21,1]
+    l=[5,2,4,6,1,3]
     print 'before:',l
     l=insert_sort(l)
     print l
