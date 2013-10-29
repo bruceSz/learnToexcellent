f4875b8a69a26f0d0fbe0decc3d630c2eb4ea16c
bruceSz <zsyuyizhang@gmail.com>
2013-10-29 18:48:34 +0800

    modified:   .path_progress 	modified:   about_array_assignment.rb 	modified:   about_hashes.rb

--- ruby/koans/about_hashes.rb
+++ ruby/koans/about_hashes.rb
@@ -3,27 +3,27 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 class AboutHashes < Neo::Koan
   def test_creating_hashes
     empty_hash = Hash.new
-    assert_equal __, empty_hash.class
-    assert_equal(__, empty_hash)
-    assert_equal __, empty_hash.size
+    assert_equal Hash, empty_hash.class
+    assert_equal({}, empty_hash)
+    assert_equal 0, empty_hash.size
   end
 
   def test_hash_literals
     hash = { :one => "uno", :two => "dos" }
-    assert_equal __, hash.size
+    assert_equal 2, hash.size
   end
 
   def test_accessing_hashes
     hash = { :one => "uno", :two => "dos" }
-    assert_equal __, hash[:one]
-    assert_equal __, hash[:two]
-    assert_equal __, hash[:doesnt_exist]
+    assert_equal "uno", hash[:one]
+    assert_equal "dos", hash[:two]
+    assert_equal nil, hash[:doesnt_exist]
   end
 
   def test_accessing_hashes_with_fetch
     hash = { :one => "uno" }
-    assert_equal __, hash.fetch(:one)
-    assert_raise(___) do
+    assert_equal "uno", hash.fetch(:one)
+    assert_raise(IndexError_) do
       hash.fetch(:doesnt_exist)
     end
 
@@ -36,7 +36,7 @@ class AboutHashes < Neo::Koan
     hash = { :one => "uno", :two => "dos" }
     hash[:one] = "eins"
 
-    expected = { :one => __, :two => "dos" }
+    expected = { :one => "eins", :two => "dos" }
     assert_equal __, hash
 
     # Bonus Question: Why was "expected" broken out into a variable
