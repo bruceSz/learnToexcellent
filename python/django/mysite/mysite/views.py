7a6580ee48d1270ac301d2dfe72fa0c7f1b07479
bruceSz <bjsongz@cn.ibm.com>
2014-03-03 15:00:37 +0800

    deleted:    python/django/mysite/mysite/__init__.pyc 	deleted:    python/django/mysite/mysite/settings.pyc 	modified:   python/django/mysite/mysite/urls.py 	deleted:    python/django/mysite/mysite/urls.pyc 	modified:   python/django/mysite/mysite/views.py 	deleted:    python/django/mysite/mysite/views.pyc 	deleted:    python/django/mysite/mysite/wsgi.pyc

--- python/django/mysite/mysite/views.py
+++ python/django/mysite/mysite/views.py
@@ -2,12 +2,23 @@ import datetime
 
 from django.http import HttpResponse
 
+
+
 def hello(request):
     return HttpResponse("hello world")
 
 def current_time(request):
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" %now
+    return HttpResponse(html)
+
+def hours_ahead(request,offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = "<html><body>In %s hour(s), it will be %s.</body><html>." %(offset,dt)
     return HttpResponse(html)
 
 
