6b829d840042734cd095d7dcf40747434bfc4171
bruceSz <zsyuyizhang@gmail.com>
2014-03-20 22:50:47 +0800

    more on webob

--- python/learn-webob/wiki.py
+++ python/learn-webob/wiki.py
@@ -12,9 +12,94 @@ class WikiApp(object):
     def __call__(self,environ,start_response):
         #
         req = Request(environ)
-        resp = Response(
-            'Hello %s!' %req.params.get('name','World'))
+        action = req.params.get('action','view')
+
+        page = self.get_page(req.path_info)
+
+        try:
+            try:
+                meth = getattr(self,'action_%s_%s',%(action,req.method))
+            except AttributeError:
+                raise exc.HTTPBadRequest('No such action %r',%action)
+            resp = meth(req,page)
+        except exc.HTTPException ,e:
+            resp = e
         return resp(environ,start_response)
+
+
+    def get_page(self,path):
+        path = path.lstrip('/')
+        if not path:
+            path = 'index'
+        path = os.path.join(self.storage_dir)
+        path = os.path.normpath(path)
+        if path.endswith('/'):
+            path +='index'
+        if not path.startswith(self.storage_dir):
+            raise exc.HTTPBadRequest("Bad path")
+        path += '.html'
+        return Page(path)
+            
+
+
+class Page(object):
+    def __init__(self,filename):
+        self.filename = filename
+
+    @property
+    def exists(self):
+        return os.path.exists(self.filename)
+
+    @property
+    def title(self):
+        if not self.exists:
+            basename = os.path.splitext(os.path.basename(self.filename))[0]
+            basename = re.sub(r'[_-]','',basename)
+            return basename.capitalize()
+        content = self.full_content
+        match = re.search(r'<title>(.*?)</title>',content,re.I|re.S)
+        return match.group(1)
+
+    @property
+    def full_content(self):
+        f = open(self.filename,'rb')
+        try:
+            return f.read()
+        finally:
+            f.close()
+
+
+    @property
+    def full_content(self):
+        f = open(self.filename,'rb')
+        try:
+            return f.read()
+        finally:
+            f.close()
+
+    @property
+    def content(self):
+        if not self.exists:
+            return ''
+        content = self.full_content
+        match = re.search(r'<body>[^>]*></body>',content,re.I|re.S)
+        return match.group(1)
+
+    @property
+    def mtime(self):
+        if not self.exists:
+            return None
+        else:
+            return int(os.stat(self.filename).st_mtime)
+
+    def set(self,title,content):
+        dir = os.path.dirname(self.filename)
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+        new_content = """<html><head><title>%s</title></head></html>"""%(title,content)
+        f = open(self.filename,'wb')
+        f.write(new_content)
+        f.close()
 
 
 if __name__ == '__main__':
