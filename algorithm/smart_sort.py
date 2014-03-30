27ad3b79b4c9b817e94c7ca913bde1ef4c6fb83f
bruceSz <zsyuyizhang@gmail.com>
2014-03-31 00:12:03 +0800

    smart sort half done. can't work well yet

---
+++ algorithm/smart_sort.py
@@ -0,0 +1,39 @@
+def smart_sort(L):
+    index1=1
+    index3=len(L)-1 
+    print index1,index3
+    while L[index1]== 1:
+        index1 += 1
+    while L[index3] ==3:
+        index3 -= 1
+
+    while index1<index3:
+        print L
+        if L[index1]==3:
+            L[index1],L[index3] = L[index3],L[index1]
+            index3 -= 1
+        else:
+            index2 = index1+1
+            print index2
+            while L[index2] != 1 and index2 < index3:
+                index2 += 1
+
+            L[index1],L[index2] = L[index2],L[index1]
+
+        while L[index1] == 1:
+            index1 += 1
+
+    return L
+
+
+
+    
+
+def test_smart_sort():
+    a=[1,2,3,2,1,2,3,2,1,2,2,2,3,3,2,1,1,1,3]
+    sorted_a = smart_sort(a)
+    print sorted_a
+
+
+if __name__ == '__main__':
+    test_smart_sort()
