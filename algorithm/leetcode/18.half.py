0e74731455066143e29e6ace31f76d4db83fd754
bruceSz <zsyuyizhang@gmail.com>
2014-07-02 22:58:32 +0800

    leetcode: candy

---
+++ algorithm/leetcode/18.half.py
@@ -0,0 +1,31 @@
+class Solution:
+    def candy(self,ratings):
+        l_low_points = []
+        candies = []
+        pre_ = None
+        next_ = None
+        length = len(ratings)
+        if length == 1:
+            return 1
+
+        for index in xrange(length):
+            if index == length-1:
+                if ratings[index-1] >= ratings[index]:
+                    l_low_points.append(index)
+                    
+            elif index == 0:
+                if ratings[1] >= ratings[0]:
+                    l_low_points.append(index)
+            else:
+                if ratings[index-1]>=ratings[index] and ratings[index+1]>=ratings[index]:
+                    l_low_points.append(index)
+            for index in xrange(length):
+                candies[index]=1
+
+            
+
+
+                
+            
+        
+
--- algorithm/leetcode/18.py
+++ algorithm/leetcode/18.py
@@ -1,31 +1,42 @@
 class Solution:
     def candy(self,ratings):
-        l_low_points = []
-        candies = []
-        pre_ = None
-        next_ = None
         length = len(ratings)
+
         if length == 1:
             return 1
+        if length == 0:
+            return 0
 
-        for index in xrange(length):
-            if index == length-1:
-                if ratings[index-1] >= ratings[index]:
-                    l_low_points.append(index)
-                    
-            elif index == 0:
-                if ratings[1] >= ratings[0]:
-                    l_low_points.append(index)
-            else:
-                if ratings[index-1]>=ratings[index] and ratings[index+1]>=ratings[index]:
-                    l_low_points.append(index)
-            for index in xrange(length):
-                candies[index]=1
+        candies = [0]*length
 
+        if  ratings[1] >= ratings[0]:
+            candies[0]=1
+        if ratings[length-2] >= ratings[length-1]:
+            candies[length-1] = 1
+
+        for i in xrange(1,length-1):
+            if ratings[i-1] >= ratings[i] and ratings[i] <= ratings[i+1]:
+                candies[i] = 1
+
+        for i in xrange(1,length):
+            if ratings[i]> ratings[i-1]:
+                candies[i] = candies[i-1]+1
             
-
-
-                
             
+        for i in xrange(length-2,-1,-1):
+            if ratings[i] > ratings[i+1]:
+                if (candies[i+1]+1)> candies[i]:
+                    candies[i]=candies[i+1]+1
+
+        ret = 0
+        print candies
+        for i in xrange(0,length):
+            ret += candies[i]
+
+        return ret
         
 
+if __name__ == '__main__':
+    ratings = [1,0,2]
+    ss = Solution()
+    print ss.candy(ratings)
