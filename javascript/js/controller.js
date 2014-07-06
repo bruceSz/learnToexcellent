8e50cb108240fbde7e263fd87c1dcc219c233946
bruceSz <zsyuyizhang@gmail.com>
2014-07-06 13:09:16 +0800

    angular class 3

---
+++ javascript/js/controller.js
@@ -0,0 +1,11 @@
+function PhoneListCtrl($scope){
+    $scope.phones = [
+        {"name":"Nexus s",
+        "snippet":"Fast just got faster with Nexus S."},
+        {"name":"Motorola  XOOM with Wi-Fi",
+        "snippet":"The Next ,Next generation tablet."},
+        {"name":"Motorola XOOM",
+        "snippet": "The Next Next generation tablet."}
+    ];
+    $scope.hello = "Hello,World!"
+}
