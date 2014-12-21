cb5e6444810e86a749ad0de88f28f980b421de76
bruseSz <zsyuyizhang@gmail.com>
2014-12-21 16:23:43 +0800

    setup learn

---
+++ python/learn-setup/foo/foo.py
@@ -0,0 +1,4 @@
+print 'hello , foo!!!'
+
+def hello():
+    print 'this is in hello function of foo module'
--- python/learn-setup/setup.py
+++ python/learn-setup/setup.py
@@ -1,5 +1,5 @@
-from distutils.core import setup
+from setuptools import setup,find_packages
 setup(name='foo',
 	version='1.0',
-	py_modules=['foo'],
+    packages = find_packages(),
 	)
